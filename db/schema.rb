cc38071ffdc09642600a569e2127a27072bb6f80
naomijoyce <naomijoyceb@gmail.com>
2019-02-22 12:17:40 -0500

    migrated tables

---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_02_22_031936) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "designs", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_designs_on_event_id"
+    t.index ["user_id"], name: "index_designs_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.string "name"
+    t.string "host"
+    t.date "date"
+    t.time "time"
+    t.integer "phone_number"
+    t.string "location"
+    t.string "address"
+    t.string "message"
+    t.bigint "user_id"
+    t.bigint "design_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["design_id"], name: "index_invites_on_design_id"
+    t.index ["event_id"], name: "index_invites_on_event_id"
+    t.index ["user_id"], name: "index_invites_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthday"
+    t.string "pw_digest"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "designs", "events"
+  add_foreign_key "designs", "users"
+  add_foreign_key "invites", "designs"
+  add_foreign_key "invites", "events"
+  add_foreign_key "invites", "users"
+end
