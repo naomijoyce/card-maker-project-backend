82fcfc3aff2bdf0299d85cd6ad8d190aa0b2f296
naomijoyce <naomijoyceb@gmail.com>
2019-02-25 01:40:22 -0500

    successful token creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
   before_action :find_user, only:[:show, :update, :destroy]
 
   def index
@@ -12,7 +13,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created 
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created 
     else
       render json: {error: 'failed to create user'}, status: :not_acceptable
     end
